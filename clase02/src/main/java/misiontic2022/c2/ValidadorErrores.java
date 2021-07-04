0fbda7cb666499f771819ba0e0adf3dbc41bd890
yo-huaca95 <yo.huaca@udla.edu.co>
2021-07-04 17:20:53 -0500

    segunda actualizacion

--- clase02/src/main/java/misiontic2022/c2/ValidadorErrores.java
+++
@@ -1,9 +0,0 @@
-package misiontic2022.c2;
-
-public class ValidadorErrores {
-
-    public static void ValorInvalido(int a) {
-        System.out.println("El valor es invalido para la operación enviaste :"+a);
-    }
-    
-}
