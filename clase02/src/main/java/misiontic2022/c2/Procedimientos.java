d20ca0eab5d56c0bb5c50923571697862fc69933
yo-huaca95 <yo.huaca@udla.edu.co>
2021-07-04 16:48:09 -0500

    Primer Actualizacion

---
+++ clase02/src/main/java/misiontic2022/c2/Procedimientos.java
@@ -0,0 +1,12 @@
+package misiontic2022.c2;
+
+
+public class Procedimientos {
+
+    public static int Segundo(int a){
+
+            int result= a/100;
+            return result;
+}
+    
+}
---
+++ clase02/src/main/java/misiontic2022/c2/ValidadorErrores.java
@@ -0,0 +1,9 @@
+package misiontic2022.c2;
+
+public class ValidadorErrores {
+
+    public static void ValorInvalido(int a) {
+        System.out.println("El valor es invalido para la operación enviaste :"+a);
+    }
+    
+}
